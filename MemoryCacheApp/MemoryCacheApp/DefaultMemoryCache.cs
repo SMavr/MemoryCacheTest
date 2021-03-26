d5aae3052da1b9eb6935d809ee3bde85387c2979
smavrikis <smavrikis@BNFT-WEBPC6>
2021-03-26 17:10:48 +0200

    Implemented basic default cache

---
+++ MemoryCacheApp/MemoryCacheApp/DefaultMemoryCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryCacheApp
+{
+    public class DefaultMemoryCache
+    {
+        private readonly MemoryCache memoryCache;
+        private readonly CacheItemPolicy cacheItemPolicy;
+
+        public DefaultMemoryCache()
+        {
+            this.memoryCache = new MemoryCache("Test");
+            this.cacheItemPolicy = new CacheItemPolicy()
+            {
+                SlidingExpiration = TimeSpan.FromSeconds(60)
+            };
+        }
+
+        public T Get<T>(string key)
+        {
+            return (T)memoryCache.Get(key);
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            memoryCache.Set(key, value, cacheItemPolicy);
+        }
+    }
+}
--- MemoryCacheApp/MemoryCacheApp/Program.cs
+++ MemoryCacheApp/MemoryCacheApp/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace MemoryCacheApp
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
+            var memoryCache = new DefaultMemoryCache();
+            var person = new Person()
+            {
+                FirstName = "Test",
+                LastName = "Testy"
+            };
+
+            memoryCache.Set("person1", person);
+
+            var personFromCache = memoryCache.Get<Person>("person1");
+
+            Console.WriteLine($"From DefaultCache: {personFromCache}");
+            Console.ReadLine();
         }
     }
 }
