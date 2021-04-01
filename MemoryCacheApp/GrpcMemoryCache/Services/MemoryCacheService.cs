4ef8ce3fd5438fccc915190e9bed6cd929cf13d8
smavrikis <smavrikis@BNFT-WEBPC6>
2021-04-01 14:03:02 +0300

    Added protobuf

---
+++ MemoryCacheApp/GrpcMemoryCache/Services/MemoryCacheService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using GrpcMemoryCache.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcMemoryCache.Services
+{
+    public class MemoryCacheService : MemoryCache.MemoryCacheBase
+    {
+        public override Task<CacheSavedReply> Save(CacheKeyValue request, ServerCallContext context)
+        {
+            return base.Save(request, context);
+        }
+
+        public override Task<CacheValue> Get(CacheKey request, ServerCallContext context)
+        {
+            return base.Get(request, context);
+        }
+    }
+}
